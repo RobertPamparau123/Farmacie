3934867fe2e616feb47a5260802c051fd726ed7b
RobertPamparau <robert.pamparau@assist.ro>
2023-04-28 12:22:10 +0300

    Pregatire pentru test

--- Farmacie/FiguraGeometrica.cs
+++ Farmacie/FiguraGeometrica.cs
@@ -11,24 +11,27 @@ namespace Farmacie
         string TipFigura;
         int  NrLaturi;
         List<int> LungimiLaturi;
+        public DateTime hour;
 
         public FiguraGeometrica()
         {
             TipFigura = "patrat";
             NrLaturi = 4;
             LungimiLaturi = new List<int>();
+            hour = new DateTime(2000,5,7, 12, 30, 0);
         }
 
-        public FiguraGeometrica(string _TipFigura,int _NrLaturi,string _LungimiLaturi)
+        public FiguraGeometrica(string _TipFigura,int _NrLaturi,string _LungimiLaturi,DateTime _hour)
         {
             TipFigura = _TipFigura;
             NrLaturi = _NrLaturi;
             LungimiLaturi = _LungimiLaturi.Split(' ').Select(Int32.Parse).ToList() ;
+            hour = _hour.Date;
         }
 
         public string Info()
         {
-            return $"Figura:{TipFigura},NrLaturi:{NrLaturi},Lungimi:{LungimiLaturi}";
+            return $"Figura:{TipFigura},NrLaturi:{NrLaturi},Lungimi:{LungimiLaturi},Hour:{hour}";
         }
 
         public int Latura1()
--- Farmacie/Program.cs
+++ Farmacie/Program.cs
@@ -21,29 +21,31 @@ namespace Farmacie
 
             //	Instantierea unui obiect de tip FiguraGeom utilizand constructorul cu parametri
             //	Tipul variabilei f1 este explicit 'FiguraGeom'
-            var figura = new FiguraGeometrica();
-            string f7 = figura.Info();
-            var figura1 = new FiguraGeometrica("Dreptunghi", 4, "3 5 3 5");
-            string f9 = figura1.Info();
-            int latura = figura1.Latura1();
-            int perimetru = figura1.Perimetru();
-            Console.WriteLine(latura);
-            Console.WriteLine(perimetru);
-            var s = new Sondaj();
-            string f = s.Info();
-            Console.WriteLine(f);
+            /* var figura = new FiguraGeometrica();
+             string f7 = figura.Info();
+             Console.WriteLine(figura.hour.TimeOfDay);
+             var figura1 = new FiguraGeometrica("Dreptunghi", 4, "3 5 3 5",new DateTime());
+             string f9 = figura1.Info();
+             Console.WriteLine(f9);
+             int latura = figura1.Latura1();
+             int perimetru = figura1.Perimetru();
+             Console.WriteLine(latura);
+             Console.WriteLine(perimetru);
+             var s = new Sondaj();
+             string f = s.Info();
+             Console.WriteLine(f);
 
-            var s5 = new Sondaj(12,17,80, EnumParerePersoana.Obez);
-            float varsta1 = s.getVarsta();
-            string interval = s5.IntervalVarsta(varsta1);
-            float greutate = s5.GreutateIdeala();
-            var f5 = s5.Info();
-            Console.WriteLine(f5);
-            Console.WriteLine(interval);
-            Console.WriteLine(greutate);
+             var s5 = new Sondaj(12,17,80, EnumParerePersoana.Obez);
+             float varsta1 = s.getVarsta();
+             string interval = s5.IntervalVarsta(varsta1);
+             float greutate = s5.GreutateIdeala();
+             var f5 = s5.Info();
+             Console.WriteLine(f5);
+             Console.WriteLine(interval);
+             Console.WriteLine(greutate);*/
 
 
-            AddMedicament f1 = new AddMedicament("Algocalmin", "Folosit pentru dureri","Pastile",456);
+            AddMedicament f1 = new AddMedicament("Algocalmin", "Folosit pentru dureri", "Pastile", 456);
             string s1 = f1.Info();
             Console.WriteLine(s1);
             string numeFisier = ConfigurationManager.AppSettings["NumeFisier"];
@@ -56,26 +58,26 @@ namespace Farmacie
 
             //Citire de la tastatura
             AddMedicament medicament = new AddMedicament();
-           
+
             //Console.WriteLine("Introduceti denumirea medicamentului:");
             string denumire = Console.ReadLine();
 
-           // Console.WriteLine("Introduceti prospectul medicamentului:");
+            // Console.WriteLine("Introduceti prospectul medicamentului:");
             string prospect = Console.ReadLine();
 
             //Console.WriteLine("Introduceti tipul medicamentului:");
             string tip = Console.ReadLine();
 
-           // Console.WriteLine("Introduceti pretul medicamentului:");
+            // Console.WriteLine("Introduceti pretul medicamentului:");
             float.TryParse(Console.ReadLine(), out float pret);
 
             //Incarcare in fisier
-            AddMedicament f2 = new AddMedicament(denumire,prospect,tip,pret);
+            AddMedicament f2 = new AddMedicament(denumire, prospect, tip, pret);
             adminStudenti.AddMedicament(f2);
-            
 
 
-            
+
+
             int nrMedicamente;
             AddMedicament[] medicamente = adminStudenti.GetMedicamente(out nrMedicamente);
 
@@ -86,8 +88,9 @@ namespace Farmacie
             }
             Console.WriteLine("Data saved to file.");
             string s2 = f2.Info();
-           //Console.WriteLine(s2);
-            //Console.ReadKey();
+            Console.WriteLine(s2);
+            Console.ReadKey();
+
         }
     }
 }
