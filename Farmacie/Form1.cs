c199e0c071367912d0bf8e6189ed5e7acfd74767
RobertPamparau <robert.pamparau@assist.ro>
2023-05-26 15:07:06 +0300

    Adaugare Forms

---
+++ Farmacie/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Farmacie
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Create an instance of the AddMedicament class
+            AddMedicament med = new AddMedicament("Aspirin", "https://www.example.com/aspirin", "Tablete", 10.50f);
+
+            // Set the Text property of each label to the corresponding field value of the med object
+            Denumire.Text = med.getDenumire();
+           // Prospect.Text = med.prospect;
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Farmacie/Program.cs
+++ Farmacie/Program.cs
@@ -13,38 +13,6 @@ namespace Farmacie
     {
         static void Main()
         {
-            //	Instantierea unui obiect de tip FiguraGeom utilizand constructorul fara parametri
-            //	Tipul variabilei f este 'var' (determinat de compilator)
-            // var f = new AddMedicament();
-            //string s = f.Info();
-            //Console.WriteLine(s);
-
-            //	Instantierea unui obiect de tip FiguraGeom utilizand constructorul cu parametri
-            //	Tipul variabilei f1 este explicit 'FiguraGeom'
-            /* var figura = new FiguraGeometrica();
-             string f7 = figura.Info();
-             Console.WriteLine(figura.hour.TimeOfDay);
-             var figura1 = new FiguraGeometrica("Dreptunghi", 4, "3 5 3 5",new DateTime());
-             string f9 = figura1.Info();
-             Console.WriteLine(f9);
-             int latura = figura1.Latura1();
-             int perimetru = figura1.Perimetru();
-             Console.WriteLine(latura);
-             Console.WriteLine(perimetru);
-             var s = new Sondaj();
-             string f = s.Info();
-             Console.WriteLine(f);
-
-             var s5 = new Sondaj(12,17,80, EnumParerePersoana.Obez);
-             float varsta1 = s.getVarsta();
-             string interval = s5.IntervalVarsta(varsta1);
-             float greutate = s5.GreutateIdeala();
-             var f5 = s5.Info();
-             Console.WriteLine(f5);
-             Console.WriteLine(interval);
-             Console.WriteLine(greutate);*/
-
-
             AddMedicament f1 = new AddMedicament("Algocalmin", "Folosit pentru dureri", "Pastile", 456);
             string s1 = f1.Info();
             Console.WriteLine(s1);
@@ -59,21 +27,22 @@ namespace Farmacie
             //Citire de la tastatura
             AddMedicament medicament = new AddMedicament();
 
-            //Console.WriteLine("Introduceti denumirea medicamentului:");
+            Console.WriteLine("Introduceti denumirea medicamentului:");
             string denumire = Console.ReadLine();
 
-            // Console.WriteLine("Introduceti prospectul medicamentului:");
+             Console.WriteLine("Introduceti prospectul medicamentului:");
             string prospect = Console.ReadLine();
 
-            //Console.WriteLine("Introduceti tipul medicamentului:");
+            Console.WriteLine("Introduceti tipul medicamentului:");
             string tip = Console.ReadLine();
 
-            // Console.WriteLine("Introduceti pretul medicamentului:");
+             Console.WriteLine("Introduceti pretul medicamentului:");
             float.TryParse(Console.ReadLine(), out float pret);
 
             //Incarcare in fisier
             AddMedicament f2 = new AddMedicament(denumire, prospect, tip, pret);
             adminStudenti.AddMedicament(f2);
+            
 
 
 
@@ -89,6 +58,7 @@ namespace Farmacie
             Console.WriteLine("Data saved to file.");
             string s2 = f2.Info();
             Console.WriteLine(s2);
+            adminStudenti.GetMedicamente1();
             Console.ReadKey();
 
         }
